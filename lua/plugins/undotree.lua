c276a7fe956ba61a1e721c1ce493e242dd650257
Malcolm Brown <malcolm.brown@brcon.net>
2026-01-24 20:30:20 -0500

    comment for undotree

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,10 +1,11 @@
 return {
-    "mbbill/undotree",
-    config = function ()
-        vim.keymap.set("n", "<leader>ut", vim.cmd.UndotreeToggle)
-        -- vim.g.undotree_DiffCommand = "FC"
-        vim.g.undotree_SetFocusWhenToggle = 1
-        vim.opt.undofile = true
-        vim.opt.undodir = vim.fn.stdpath("data") .. "/undo"
-    end
+	"mbbill/undotree",
+	config = function()
+		vim.keymap.set("n", "<leader>ut", vim.cmd.UndotreeToggle)
+		-- for windows
+		-- vim.g.undotree_DiffCommand = "FC"
+		vim.g.undotree_SetFocusWhenToggle = 1
+		vim.opt.undofile = true
+		vim.opt.undodir = vim.fn.stdpath("data") .. "/undo"
+	end,
 }
