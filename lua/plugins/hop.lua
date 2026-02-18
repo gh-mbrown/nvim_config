56d2146e61db19339e2b8c914e68821b572263bf
Malcolm Brown <malcolm.brown@brcon.net>
2026-02-17 19:53:35 -0500

    reversing the color move

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -36,33 +36,5 @@ return {
 		end)
 
 		hop.setup({})
-
-		-- changes the color of the characters when hopping
-		vim.api.nvim_set_hl(0, "HopNextKey", {
-			bg = "NONE",
-			fg = "#ff007c",
-			bold = true,
-		})
-
-		vim.api.nvim_set_hl(0, "HopNextKey1", {
-			bg = "NONE",
-			fg = "#00dfff",
-			bold = true,
-		})
-		vim.api.nvim_set_hl(0, "HopNextKey2", {
-			bg = "NONE",
-			fg = "#2b8db3",
-			bold = true,
-		})
-
-		vim.api.nvim_set_hl(0, "LineNr", {
-			fg = "#ebbcba",
-		})
-		vim.api.nvim_set_hl(0, "LineNrBelow", {
-			fg = "#6e6a86",
-		})
-		vim.api.nvim_set_hl(0, "LineNrAbove", {
-			fg = "#6e6a86",
-		})
 	end,
 }
