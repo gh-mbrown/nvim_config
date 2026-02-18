cc2bf7c8393c44453e75662c93ca868692014a5f
Malcolm Brown <malcolm.brown@brcon.net>
2026-02-17 19:52:16 -0500

    moving the color changer for hop plugin

--- init.lua
+++ init.lua
@@ -4,30 +4,3 @@ require("config")
 require("core.lsp").setup()
 require("core.diagnostics")
 require("languages")
-
-vim.api.nvim_set_hl(0, "HopNextKey", {
-	bg = "NONE",
-	fg = "#ff007c",
-	bold = true,
-})
-
-vim.api.nvim_set_hl(0, "HopNextKey1", {
-	bg = "NONE",
-	fg = "#00dfff",
-	bold = true,
-})
-vim.api.nvim_set_hl(0, "HopNextKey2", {
-	bg = "NONE",
-	fg = "#2b8db3",
-	bold = true,
-})
-
-vim.api.nvim_set_hl(0, "LineNr", {
-	fg = "#ebbcba",
-})
-vim.api.nvim_set_hl(0, "LineNrBelow", {
-	fg = "#6e6a86",
-})
-vim.api.nvim_set_hl(0, "LineNrAbove", {
-	fg = "#6e6a86",
-})
