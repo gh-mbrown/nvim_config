5bfbdd0c2bc79db297708d0ed48f0f1d59202b9b
Malcolm Brown <malcolm.brown@brcon.net>
2026-03-27 23:01:30 -0400

    updates

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,7 +3,6 @@ return {
 	name = "catppuccin",
 	priority = 1000,
 	config = function()
-		local mocha = require("catppuccin.palettes").get_palette("mocha")
 		require("catppuccin").setup({
 			flavour = "mocha",
 			transparent_background = true,
@@ -11,6 +10,8 @@ return {
 				solid = true,
 			},
 			term_colors = false,
+			default_integrations = false,
+			auto_integrations = true,
 			integrations = {
 				blink_cmp = {
 					style = "bordered",
@@ -23,39 +24,14 @@ return {
 				},
 				which_key = true,
 				lualine = {
-					normal = {
-						a = { bg = mocha.blue, fg = mocha.mantle, gui = "bold" },
-						b = { bg = mocha.surface0, fg = mocha.blue },
-						c = { bg = "none", fg = mocha.text },
-					},
-
-					insert = {
-						a = { bg = mocha.green, fg = mocha.base, gui = "bold" },
-						b = { bg = mocha.surface0, fg = mocha.green },
-					},
-
-					terminal = {
-						a = { bg = mocha.green, fg = mocha.base, gui = "bold" },
-						b = { bg = mocha.surface0, fg = mocha.green },
-					},
-
-					command = {
-						a = { bg = mocha.peach, fg = mocha.base, gui = "bold" },
-						b = { bg = mocha.surface0, fg = mocha.peach },
-					},
-					visual = {
-						a = { bg = mocha.mauve, fg = mocha.base, gui = "bold" },
-						b = { bg = mocha.surface0, fg = mocha.mauve },
-					},
-					replace = {
-						a = { bg = mocha.red, fg = mocha.base, gui = "bold" },
-						b = { bg = mocha.surface0, fg = mocha.red },
-					},
-					inactive = {
-						a = { bg = "none", fg = mocha.blue },
-						b = { bg = "none", fg = mocha.surface1, gui = "bold" },
-						c = { bg = "none", fg = mocha.overlay0 },
-					},
+					all = function(colors)
+						---@type CtpIntegrationLualineOverride
+						return {
+							normal = {
+								c = { bg = colors.surface0 },
+							},
+						}
+					end,
 				},
 			},
 		})
