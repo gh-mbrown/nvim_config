2981a1840e5b3c8ea30035df53e55f89a9aa5208
Malcolm Brown <malcolm.brown@brcon.net>
2026-05-09 16:21:28 -0400

    updates

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,6 +6,17 @@ return {
 		require("catppuccin").setup({
 			flavour = "mocha",
 			transparent_background = true,
+			custom_highlights = {
+				CursorLine = {
+					bg = "none",
+				},
+			},
+			integrations = {
+				hop = true,
+				which_key = true,
+				mason = true,
+				harpoon = true,
+			},
 		})
 		vim.cmd.colorscheme("catppuccin-nvim")
 	end,
