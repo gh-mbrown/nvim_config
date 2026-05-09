3f29a9435de5adce4c54948b1dbcd34153c883f6
Malcolm Brown <malcolm.brown@brcon.net>
2026-05-08 23:20:21 -0400

    update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,20 +1,14 @@
 return {
-	"sainnhe/gruvbox-material",
+	"ayu-theme/ayu-vim",
 	config = function()
-		vim.cmd.colorscheme("gruvbox-material")
+		vim.g.ayucolor = "dark"
+		vim.cmd.colorscheme("ayu")
+
+		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "TelescopeNormal", { link = "NormalFloat" })
+		vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+		vim.api.nvim_set_hl(0, "CursorLine", { bg = "none" })
+		vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "none", fg = "#f29718" })
+		vim.api.nvim_set_hl(0, "HopNextKey", { cterm = { bold = true }, ctermfg = 198, bold = true, fg = "#ff7733" })
 	end,
 }
--- return {
--- 	"ayu-theme/ayu-vim",
--- 	config = function()
--- 		vim.g.ayucolor = "dark"
--- 		vim.cmd.colorscheme("ayu")
---
--- 		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
--- 		vim.api.nvim_set_hl(0, "TelescopeNormal", { link = "NormalFloat" })
--- 		vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
--- 		vim.api.nvim_set_hl(0, "CursorLine", { bg = "none" })
--- 		vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "none", fg = "#f29718" })
--- 		vim.api.nvim_set_hl(0, "HopNextKey", { cterm = { bold = true }, ctermfg = 198, bold = true, fg = "#ff7733" })
--- 	end,
--- }
