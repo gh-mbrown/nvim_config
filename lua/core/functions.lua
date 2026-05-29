617b202898308f654fa5db5ba2ad827598b8e714
Malcolm Brown <malcolm.brown@brcon.net>
2026-05-28 22:03:04 -0400

    updates

--- lua/core/functions.lua
+++ lua/core/functions.lua
@@ -12,7 +12,7 @@ local function close_buffer()
 		vim.cmd("quit")
 	end
 end
-local function checkout_branch()
+local function create_branch()
 	local branch = vim.fn.input("Branch name: ")
 	if branch ~= "" then
 		vim.cmd.Git("checkout -b " .. branch)
@@ -20,22 +20,21 @@ local function checkout_branch()
 	end
 end
 local function restore_file()
-	local file = vim.fn.expand("%")
-	local confirm = vim.fn.input("Do you want to restore this file(yes/no): ")
-	if confirm == "yes" then
-		vim.cmd.Git("restore " .. file)
+	local list = vim.fn.systemlist("git diff --name-only | awk '{ print $1 }'")
+	if #list == 0 then
+		vim.notify("No changed git files", vim.log.levels.WARN)
+		return
 	end
+	vim.ui.select(list, { prompt = "Restore File: " }, function(choice)
+		if choice then
+			vim.cmd.Git("restore " .. choice)
+		end
+	end)
 end
 local function restore_all_files()
 	local confirm = vim.fn.input("Do you want to restore ALL files(yes/no): ")
 	if confirm == "yes" then
 		vim.cmd.Git("restore :/")
-	end
-end
-local function rebase_branch()
-	local branch = vim.fn.input("Branch to rebase on: ")
-	if branch ~= "" then
-		vim.cmd.Git("rebase " .. branch)
 	end
 end
 local function print_conflicts()
@@ -49,11 +48,50 @@ local function stash_changes()
 	vim.cmd.Git("stash push --message '" .. message .. "' " .. file)
 end
 local function read_cmd(cmd)
+	if cmd == "" then
+		return
+	end
 	vim.cmd("botright new | term " .. cmd)
 end
+local function update_submodules()
+	local list = vim.fn.systemlist("git config --file .gitmodules --get-regexp path | awk '{ print $2 }'")
+	if #list == 0 then
+		vim.notify("No git modules", vim.log.levels.WARN)
+		return
+	end
+	vim.ui.select(list, { prompt = "Update Submodule:" }, function(choice)
+		if choice then
+			vim.cmd.Git("submodule update --remote " .. choice)
+		end
+	end)
+end
+local function rebase_branch()
+	local list = vim.fn.systemlist("git branch | awk '{ print $2 }'")
+	if #list == 0 then
+		vim.notify("No git branches", vim.log.levels.WARN)
+		return
+	end
+	vim.ui.select(list, { prompt = "Rebase Branch: " }, function(choice)
+		if choice then
+			vim.cmd.Git("rebase " .. choice)
+		end
+	end)
+end
+local function git_add()
+	local list = vim.fn.systemlist("git diff --name-only | awk '{ print $1 }'")
+	if #list == 0 then
+		vim.notify("No changed git files", vim.log.levels.WARN)
+		return
+	end
+	vim.ui.select(list, { prompt = "Add File: " }, function(choice)
+		if choice then
+			vim.cmd.Git("add " .. choice)
+		end
+	end)
+end
 
-vim.api.nvim_create_user_command("CheckoutBranch", function()
-	checkout_branch()
+vim.api.nvim_create_user_command("CreateBranch", function()
+	create_branch()
 end, {})
 vim.api.nvim_create_user_command("RestoreFile", function()
 	restore_file()
@@ -73,6 +111,12 @@ end, {})
 vim.api.nvim_create_user_command("ReadCmd", function(opts)
 	read_cmd(opts.args ~= "" and opts.args or vim.fn.input(""))
 end, { nargs = "?" })
+vim.api.nvim_create_user_command("UpdateSubmodule", function()
+	update_submodules()
+end, {})
+vim.api.nvim_create_user_command("GitAdd", function()
+	git_add()
+end, {})
 vim.api.nvim_create_user_command("CloseBuffer", function()
 	close_buffer()
 end, {})
