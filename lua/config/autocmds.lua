ac490e0735fac1a631bc44229108dab9f16169af
Malcolm Brown <malcolm.brown@brcon.net>
2026-06-24 23:06:21 -0400

    updates

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -11,11 +11,3 @@ vim.api.nvim_create_autocmd("lspattach", {
         vim.lsp.inlay_hint.enable(false, { bufnr = args.buf })
     end,
 })
-
--- Enter terminal in insert mode
-vim.api.nvim_create_autocmd({"TermOpen", "BufEnter"}, {
-    pattern = "term://*",
-    callback = function ()
-        vim.cmd("startinsert")
-    end
-})
