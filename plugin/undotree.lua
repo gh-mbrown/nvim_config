b60fd1a3a4b5eecd2306d8dd39fc26cb0d316c8c
Malcolm Brown <malcolm.brown@brcon.net>
2026-06-25 22:19:08 -0400

    updates

--- plugin/undotree.lua
+++ plugin/undotree.lua
@@ -1,10 +1,7 @@
 vim.pack.add({
-    GIT_ROOT .. "mbbill/undotree"
+    GIT_ROOT .. "jiaoshijie/undotree"
 })
-if vim.fn.has("win32") == 1 then
-    vim.g.undotree_DiffCommand = "FC"
-end
-vim.g.undotree_SetFocusWhenToggle = 1
-vim.opt.undofile = true
-vim.opt.undodir = vim.fn.stdpath("data") .. "/undo"
-vim.keymap.set("n", "<leader>ut", vim.cmd.UndotreeToggle)
+
+local ut = require("undotree")
+ut.setup({})
+vim.keymap.set("n", "<leader>ut", ut.toggle)
