afb430fc9946fddf8e7f86a30d3551ed2bf4da23
Malcolm Brown <malcolm.brown@brcon.net>
2026-06-28 13:47:30 -0400

    updates

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -5,20 +5,39 @@ vim.api.nvim_create_autocmd("PackChanged", {
             if not ev.data.active then vim.cmd.packadd("telescope-fzf-native.nvim") end
             vim.system({ "cmake", "-S.", "-Bbuild", "-DCMAKE_BUILD_TYPE=Release" }, { cwd = ev.data.path }):wait()
             vim.system({ "cmake", "--build", "build", "--config", "Release", "--target", "install" },
-            { cwd = ev.data.path }):wait()
+                { cwd = ev.data.path }):wait()
         end
     end
 })
 
 vim.pack.add({
     { src = GIT_ROOT .. "nvim-lua/plenary.nvim" },
-    { src = GIT_ROOT .. "nvim-telescope/telescope-fzf-native.nvim"},
+    { src = GIT_ROOT .. "nvim-telescope/telescope-fzf-native.nvim" },
     { src = GIT_ROOT .. "nvim-telescope/telescope.nvim" }
 })
 
 local telescope = require("telescope")
 local builtin = require("telescope.builtin")
+local actions = require("telescope.actions")
 telescope.setup({
+    defaults = {
+        sorting_strategy = "ascending",
+        layout_config = {
+            horizontal = {
+                prompt_position = "top"
+            }
+        },
+        path_display = {
+            "filename_first"
+        },
+        mappings = {
+            i = {
+                ["<Esc>"] = actions.close
+            }
+        }
+    },
+    pickers = {
+    },
     extensions = {
         fzf = {
             fuzzy = true,
@@ -30,3 +49,17 @@ telescope.setup({
 })
 telescope.load_extension("fzf")
 vim.keymap.set("n", "<leader>pf", builtin.git_files)
+vim.keymap.set("n", "<leader>ff", builtin.find_files)
+vim.keymap.set("n", "<leader>pg", builtin.live_grep)
+vim.keymap.set("v", "<leader>pg", builtin.grep_string)
+vim.keymap.set("n", "<leader>pb", builtin.buffers)
+vim.keymap.set("n", "<leader>ph", builtin.help_tags)
+vim.keymap.set("n", "<leader>ch", builtin.command_history)
+vim.keymap.set("n", "<leader>pm", builtin.man_pages)
+vim.keymap.set("n", "/", builtin.current_buffer_fuzzy_find)
+vim.keymap.set("n", "gd", builtin.lsp_definitions)
+vim.keymap.set("n", "gr", builtin.lsp_references)
+vim.keymap.set("n", "gi", builtin.lsp_implementations)
+vim.keymap.set("n", "gt", builtin.lsp_type_definitions)
+vim.keymap.set("n", "<leader>dd", builtin.diagnostics)
+vim.keymap.set("n", "<leader>pt", builtin.treesitter)
