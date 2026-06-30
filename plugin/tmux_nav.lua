29c584f5b068c627fc908b37704eb61236394c55
Malcolm Brown <malcolm.brown@brcon.net>
2026-06-29 21:00:37 -0400

    updates

--- plugin/tmux_nav.lua
+++
@@ -1,7 +0,0 @@
-require("lazy_load").on_vim_enter(function ()
-    if vim.fn.has("linux") == 1 then
-        vim.pack.add({
-            GIT_ROOT .. "christoomey/vim-tmux-navigator"
-        })
-    end
-end)
