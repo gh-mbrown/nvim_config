5d1de5ad05c443f88abae699a7176a7c21f85b03
Malcolm Brown <malcolm.brown@brcon.net>
2026-07-01 12:33:53 -0400

    updates

--- plugin/fugitive.lua
+++ plugin/fugitive.lua
@@ -3,23 +3,40 @@ require("lazy_load").on_vim_enter(function()
         GIT_ROOT .. "tpope/vim-fugitive"
     })
 
-    vim.keymap.set("n", "<leader>gg", function ()
-        vim.cmd.Git()
-        vim.cmd.only()
-    end)
+    vim.keymap.set("n", "<leader>gg", vim.cmd.Git)
     vim.keymap.set("n", "<leader>gw", function()
         vim.cmd.Git("blame")
     end)
-    vim.keymap.set("n", "<leader>gl", function ()
+    vim.keymap.set("n", "<leader>gl", function()
         vim.cmd.Git("--paginate log")
     end)
-    vim.keymap.set("n", "<leader>gd", function ()
+    vim.keymap.set("n", "<leader>gd", function()
         vim.cmd.Git("--paginate diff")
     end)
-    vim.keymap.set("n", "<leader>gm", function ()
+    vim.keymap.set("n", "<leader>gD", vim.cmd.Gvdiffsplit)
+    vim.keymap.set("n", "<leader>gm", function()
         vim.cmd.Git("mergetool")
     end)
-    vim.keymap.set("n", "<leader>gt", function ()
+    vim.keymap.set("n", "<leader>gt", function()
         vim.cmd.Git("difftool")
     end)
+    vim.keymap.set("n", "<leader>gp", function()
+        vim.cmd.Git("pull --rebase")
+    end)
+    vim.keymap.set("n", "<leader>gP", function()
+        vim.cmd.Git("push")
+    end)
+
+    vim.api.nvim_create_autocmd("FileType", {
+        pattern = "gitcommit",
+        callback = function()
+            vim.cmd.startinsert()
+        end
+    })
+    vim.api.nvim_create_autocmd("FileType", {
+        pattern = "gitcommit",
+        callback = function(args)
+            vim.keymap.set("i", "<C-c><C-c>", vim.cmd.wq, { buffer = args.buf })
+        end
+    })
 end)
